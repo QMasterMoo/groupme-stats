0d444c96d08fbc2b8f25af54db7c2f02aee1e133
Alex Harding <cpsaicontact@gmail.com>
2017-12-22 17:25:58 +0000

    message retrieval progress

--- request.py
+++ request.py
@@ -15,15 +15,28 @@ def getResponse(response):
 	"""Returns the 'response' portion of our api calls. Idea stolen from github.com/youyanggu"""
 	return response.json()['response']
 
+def getMessages(groupID, payload):
+	messagesRaw = getResponse(requests.get(api_URL + '/groups/' + groupID + '/messages' + api_key, params=payload))
+	messages = []
+	for message in messagesRaw['messages']:
+		messages.append((message['id'], message['text']))
+	return messages
+
+def getAllMessages(groupID, payload):
+	payload['limit'] = 20
+	messages = getMessages(groupID, payload)
+	allMessages = messages
+	count = len(allMessages)
+	while count == 20: #20 is defalt request size
+		payload['before_id'] = messages[0][0]
+		messages = getMessages(groupID, payload)
+		allMessages += messages
+		count = len(messages)
+	return allMessages
 
 #DO THINGS HERE
+groupID = ""
 
-
-
-
-
-payload = {'per_page' : 10}
-r = getResponse(requests.get(api_URL + "/groups" + api_key, params=payload))
-
-for group in r:
-	print(group['name'], group['id'])
+payload = {'limit' : 20}
+print(getAllMessages(groupID, payload))
+#getAllMessages(groupID, payload)
