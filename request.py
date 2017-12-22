a12123a97b4fc8396ba91198fc70d8ea0c505fc9
Alex Harding <cpsaicontact@gmail.com>
2017-12-22 16:37:06 +0000

    testing out requests

--- request.py
+++ request.py
@@ -1,1 +1,29 @@
-import requsts
+import requests
+
+#Setup api key
+api_URL = 'https://api.groupme.com/v3'
+api_key = '?token='
+with open('api.key', 'r') as f:
+	api_key += f.read()
+
+##################
+#helper functions#
+##################
+
+
+def getResponse(response):
+	"""Returns the 'response' portion of our api calls. Idea stolen from github.com/youyanggu"""
+	return response.json()['response']
+
+
+#DO THINGS HERE
+
+
+
+
+
+payload = {'per_page' : 10}
+r = getResponse(requests.get(api_URL + "/groups" + api_key, params=payload))
+
+for group in r:
+	print(group['name'], group['id'])
